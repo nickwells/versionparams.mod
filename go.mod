0c54649da237954c08ce844c153cd582dca1a75f
Nick Wells <wells.nicholas@gmail.com>
2022-05-21 09:52:20 +0100

    feature: Replace the old version info with the runtime/debug values

--- go.mod
+++ go.mod
@@ -1,9 +1,23 @@
 module github.com/nickwells/versionparams.mod
 
-go 1.15
+go 1.18
 
 require (
-	github.com/nickwells/location.mod v1.2.20
-	github.com/nickwells/param.mod/v5 v5.14.0
-	github.com/nickwells/version.mod v1.0.5
+	github.com/nickwells/col.mod/v3 v3.0.4
+	github.com/nickwells/location.mod v1.2.21
+	github.com/nickwells/param.mod/v5 v5.14.2
+)
+
+require (
+	github.com/nickwells/check.mod/v2 v2.0.1 // indirect
+	github.com/nickwells/errutil.mod v1.2.3 // indirect
+	github.com/nickwells/filecheck.mod v1.1.13 // indirect
+	github.com/nickwells/fileparse.mod v1.1.25 // indirect
+	github.com/nickwells/mathutil.mod/v2 v2.0.0 // indirect
+	github.com/nickwells/pager.mod v1.0.9 // indirect
+	github.com/nickwells/strdist.mod v1.1.5 // indirect
+	github.com/nickwells/twrap.mod v1.4.17 // indirect
+	golang.org/x/exp v0.0.0-20220407100705-7b9b53b0aca4 // indirect
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
--- versionparams/addParams.go
+++ versionparams/addParams.go
@@ -1,68 +1,207 @@
 package versionparams
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"runtime/debug"
 
+	"github.com/nickwells/col.mod/v3/col"
+	"github.com/nickwells/col.mod/v3/col/colfmt"
 	"github.com/nickwells/location.mod/location"
 	"github.com/nickwells/param.mod/v5/param"
-	"github.com/nickwells/param.mod/v5/param/paction"
 	"github.com/nickwells/param.mod/v5/param/psetter"
-	"github.com/nickwells/version.mod/version"
 )
+
+var vsnPart string
+
+const (
+	vpGoVersion = "go-version"
+	vpPath      = "path"
+	vpMods      = "modules"
+	vpSettings  = "build-settings"
+
+	noBuildInfo = "Build information not available"
+
+	replIntro   = "   "
+	modTypeDep  = "D"
+	modTypeRepl = "r"
+	modTypeMain = "M"
+)
+
+// moduleInfo returns a string describing the module
+func moduleInfo(m debug.Module) string {
+	mi := "     Path: " + m.Path
+	if m.Version != "" {
+		mi += "\n Version: " + m.Version
+	}
+	if m.Sum != "" {
+		mi += "\nCheckSum: " + m.Sum
+	}
+	if m.Replace != nil {
+		mi += "\nReplaced by:\n" + moduleInfo(*m.Replace)
+	}
+	return mi
+}
+
+// depCols returns a set of columns for the dependencies section of the
+// report with the column widths set to the maximum observed value
+func depCols(bi *debug.BuildInfo) []*col.Col {
+	var (
+		colWidthPath    = len(bi.Main.Path)
+		colWidthVersion = len(bi.Main.Version)
+		colWidthSum     = len(bi.Main.Sum)
+	)
+	for _, d := range bi.Deps {
+		if colWidthPath < len(d.Path) {
+			colWidthPath = len(d.Path)
+		}
+		if colWidthVersion < len(d.Version) {
+			colWidthVersion = len(d.Version)
+		}
+		if colWidthSum < len(d.Sum) {
+			colWidthSum = len(d.Sum)
+		}
+		if d.Replace != nil {
+			replPathLen := len(d.Replace.Path) + len(replIntro)
+			if colWidthPath < replPathLen {
+				colWidthPath = replPathLen
+			}
+			if colWidthVersion < len(d.Replace.Version) {
+				colWidthVersion = len(d.Replace.Version)
+			}
+			if colWidthSum < len(d.Replace.Sum) {
+				colWidthSum = len(d.Replace.Sum)
+			}
+		}
+	}
+
+	return []*col.Col{
+		col.New(&colfmt.String{W: colWidthPath}, "Path"),
+		col.New(&colfmt.String{W: colWidthVersion}, "Version"),
+		col.New(&colfmt.String{W: colWidthSum}, "CheckSum"),
+	}
+}
+
+// showGoVersion shows the Go Version used to build this executable
+func showGoVersion(bi *debug.BuildInfo, p *param.ByName) error {
+	if bi == nil {
+		return errors.New("Could not show the Go Version - no build info")
+	}
+	fmt.Fprintln(p.StdWriter(), bi.GoVersion)
+	return nil
+}
+
+// showPath shows the Path of this executable
+func showPath(bi *debug.BuildInfo, p *param.ByName) error {
+	if bi == nil {
+		return errors.New("Could not show the Path - no build info")
+	}
+	fmt.Fprintln(p.StdWriter(), bi.Path)
+	return nil
+}
+
+// showModules shows the module details of this executable
+func showModules(bi *debug.BuildInfo, p *param.ByName) error {
+	if bi == nil {
+		return errors.New("Could not show the dependencies - no build info")
+	}
+	fmt.Fprintln(p.StdWriter(), "Modules:")
+	cols := depCols(bi)
+	rpt := col.NewReport(nil, p.StdWriter(),
+		col.New(&colfmt.String{}, "Type"), cols...)
+	_ = rpt.PrintRow(modTypeMain, bi.Main.Path, bi.Main.Version, bi.Main.Sum)
+	for _, dep := range bi.Deps {
+		modType := modTypeDep
+		if dep.Replace != nil {
+			modType = modTypeRepl
+		}
+		_ = rpt.PrintRow(modType, dep.Path, dep.Version, dep.Sum)
+		if dep.Replace != nil {
+			repl := dep.Replace
+			_ = rpt.PrintRow(modTypeDep,
+				replIntro+repl.Path, repl.Version, repl.Sum)
+		}
+	}
+	return nil
+}
+
+// showSettings shows the environment settings of this executable
+func showSettings(bi *debug.BuildInfo, p *param.ByName) error {
+	maxKey := 0
+	for _, s := range bi.Settings {
+		if len(s.Key) > maxKey {
+			maxKey = len(s.Key)
+		}
+	}
+	rpt := col.NewReport(nil, p.StdWriter(),
+		col.New(&colfmt.String{StrJust: col.Right, W: maxKey},
+			"Key"),
+		col.New(&colfmt.String{}, "Value"))
+	fmt.Fprintln(p.StdWriter(), "Build Settings:")
+	for _, s := range bi.Settings {
+		_ = rpt.PrintRow(s.Key, s.Value)
+	}
+	return nil
+}
+
+// showVersionPart shows the selected part of the version details
+func showVersionPart(_ location.L, p *param.ByName, _ []string) error {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Fprintln(p.StdWriter(), noBuildInfo)
+		os.Exit(1)
+	}
+
+	var err error
+	switch vsnPart {
+	case vpGoVersion:
+		err = showGoVersion(bi, p)
+	case vpPath:
+		err = showPath(bi, p)
+	case vpMods:
+		err = showModules(bi, p)
+	case vpSettings:
+		err = showSettings(bi, p)
+	default:
+		badVsnPart := "bad version part: " + vsnPart
+		err = errors.New(badVsnPart)
+		fmt.Fprintln(p.StdWriter(), badVsnPart)
+	}
+	os.Exit(0)
+	return err
+}
 
 // AddParams will add parameters to the passed param.PSet
 func AddParams(ps *param.PSet) error {
 	ps.Add("version", psetter.Nil{},
 		"show the version details for this program",
-		param.PostAction(paction.ReportAndExit(version.All()+"\n")),
-		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
-	)
-
-	var vsnPart string
-	const (
-		vpTag       = "tag"
-		vpCommit    = "commit"
-		vpAuthor    = "author"
-		vpDate      = "date"
-		vpBuildDate = "build-date"
-		vpBuiltBy   = "built-by"
-	)
-	ps.Add("version-part",
-		psetter.Enum{
-			Value: &vsnPart,
-			AllowedVals: psetter.AllowedVals{
-				vpTag:       "show the version tag",
-				vpCommit:    "show the latest commit ID",
-				vpAuthor:    "show the author of the latest commit",
-				vpDate:      "show the date of the latest commit",
-				vpBuildDate: "show the build date",
-				vpBuiltBy:   "show the name of the user who built the program",
-			},
-			AllowInvalidInitialValue: true,
-		},
-		"show the named part of the version",
 		param.PostAction(
 			func(_ location.L, p *param.ByName, _ []string) error {
-				switch vsnPart {
-				case vpTag:
-					fmt.Fprintln(p.StdWriter(), version.Tag())
-				case vpCommit:
-					fmt.Fprintln(p.StdWriter(), version.Commit())
-				case vpAuthor:
-					fmt.Fprintln(p.StdWriter(), version.Author())
-				case vpDate:
-					fmt.Fprintln(p.StdWriter(), version.Date())
-				case vpBuildDate:
-					fmt.Fprintln(p.StdWriter(), version.BuildDate())
-				case vpBuiltBy:
-					fmt.Fprintln(p.StdWriter(), version.BuildUser())
-				default:
-					fmt.Fprintln(p.StdWriter(), "bad version part: "+vsnPart)
+				if bi, ok := debug.ReadBuildInfo(); !ok {
+					fmt.Fprintln(p.StdWriter(), noBuildInfo)
+				} else {
+					fmt.Fprintln(p.StdWriter(), bi.String())
 				}
 				os.Exit(0)
 				return nil
 			}),
+		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
+	)
+
+	ps.Add("version-part",
+		psetter.Enum{
+			Value: &vsnPart,
+			AllowedVals: psetter.AllowedVals{
+				vpGoVersion: "show the version of Go that produced this binary",
+				vpPath:      "show the path of the main package",
+				vpMods:      "show the module dependencies",
+				vpSettings:  "show other information about the build",
+			},
+			AllowInvalidInitialValue: true,
+		},
+		"show the named part of the version",
+		param.PostAction(showVersionPart),
 		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
 	)
 
