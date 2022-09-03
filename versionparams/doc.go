dab4708a57029d09dc11d3fdcddad59a60e2ebcc
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 23:11:54 +0100

    refactor: minor changes to keep gofumpt quiet

--- versionparams/doc.go
+++ versionparams/doc.go
@@ -1,8 +1,6 @@
 /*
-
 Package versionparams provides an AddParams function that will add the
 'version' parameter which will print the program version if passed to the
 program.
-
 */
 package versionparams
