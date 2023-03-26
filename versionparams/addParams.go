590ee152950171bf2bfd1d2dcdde6b9d849fbc00
Nick Wells <wells.nicholas@gmail.com>
2023-03-26 18:35:40 +0100

    feature: Add the version-part-short parameter
    This allows the display of the different parts of the BuildInfo to be made
    without the leading prompts or column headings. this should make it easier to
    use the value printed in the shell (to set a shell variable for instance).
    
    Also:
    - make the default display use the formatted displays
    - add some part aliases
    - add a 'raw' part to show the default display of the BuildInfo
    - prevent the same part being shown twice
    - refactor the func that shows the different parts to use a func map

--- versionparams/addParams.go
+++ versionparams/addParams.go
@@ -14,7 +14,10 @@ import (
 	"github.com/nickwells/param.mod/v5/param/psetter"
 )
 
-var vsnPart []string
+var (
+	vsnPart      []string
+	shortDisplay bool
+)
 
 const (
 	vpGoVersion = "go-version"
@@ -22,6 +25,7 @@ const (
 	vpMain      = "main"
 	vpMods      = "modules"
 	vpSettings  = "build-settings"
+	vpRaw       = "raw"
 
 	noBuildInfo = "Build information not available"
 
@@ -30,6 +34,17 @@ const (
 	modTypeRepl = "r"
 	modTypeMain = "M"
 )
+
+type versionPartShowFunc func(io.Writer, *debug.BuildInfo)
+
+var vsnPartShowFuncMap = map[string]versionPartShowFunc{
+	vpGoVersion: showGoVersion,
+	vpPath:      showPath,
+	vpMain:      showMain,
+	vpMods:      showModules,
+	vpSettings:  showSettings,
+	vpRaw:       showRaw,
+}
 
 // depCols returns a set of columns for the modules section of the report
 // with the column widths set to the maximum observed value
@@ -71,44 +86,53 @@ func depCols(bi *debug.BuildInfo) []*col.Col {
 }
 
 // showGoVersion shows the Go Version used to build this executable
-func showGoVersion(bi *debug.BuildInfo, w io.Writer) error {
-	if bi == nil {
-		return errors.New("Could not show the Go Version - no build info")
+func showGoVersion(w io.Writer, bi *debug.BuildInfo) {
+	prompt := "Built with Go Version: "
+	if shortDisplay {
+		prompt = ""
 	}
-	fmt.Fprintln(w, "Built with Go Version: ", bi.GoVersion)
-	return nil
+
+	fmt.Fprintln(w, prompt+bi.GoVersion)
 }
 
 // showPath shows the Path of this executable
-func showPath(bi *debug.BuildInfo, w io.Writer) error {
-	if bi == nil {
-		return errors.New("Could not show the Path - no build info")
+func showPath(w io.Writer, bi *debug.BuildInfo) {
+	prompt := "Path: "
+	if shortDisplay {
+		prompt = ""
 	}
-	fmt.Fprintln(w, "Path: ", bi.Path)
-	return nil
+
+	fmt.Fprintln(w, prompt+bi.Path)
 }
 
 // showMain shows the details of the main module of this executable
-func showMain(bi *debug.BuildInfo, w io.Writer) error {
-	if bi == nil {
-		return errors.New("Could not show the modules - no build info")
+func showMain(w io.Writer, bi *debug.BuildInfo) {
+	prompt := "Module, Version"
+	if bi.Main.Sum != "" {
+		prompt += ", Checksum"
+	}
+	prompt += ": "
+	if shortDisplay {
+		prompt = ""
 	}
 
-	fmt.Fprintln(w, bi.Main.Path, bi.Main.Version, bi.Main.Sum)
-
-	return nil
+	fmt.Fprintln(w, prompt+bi.Main.Path, bi.Main.Version, bi.Main.Sum)
 }
 
 // showModules shows the module details of this executable
-func showModules(bi *debug.BuildInfo, w io.Writer) error {
-	if bi == nil {
-		return errors.New("Could not show the modules - no build info")
+func showModules(w io.Writer, bi *debug.BuildInfo) {
+	if !shortDisplay {
+		fmt.Fprintln(w, "Modules:")
 	}
 
-	fmt.Fprintln(w, "Modules:")
-
 	cols := depCols(bi)
-	rpt := col.NewReport(nil, w,
+
+	hdr := col.NewHeaderOrPanic()
+	if shortDisplay {
+		col.HdrOptDontPrint(hdr)
+	}
+
+	rpt := col.NewReport(hdr, w,
 		col.New(&colfmt.String{}, "Type"), cols...)
 	_ = rpt.PrintRow(modTypeMain, bi.Main.Path, bi.Main.Version, bi.Main.Sum)
 
@@ -126,101 +150,137 @@ func showModules(bi *debug.BuildInfo, w io.Writer) error {
 				replIntro+repl.Path, repl.Version, repl.Sum)
 		}
 	}
-
-	return nil
 }
 
-// showSettings shows the environment settings of this executable
-func showSettings(bi *debug.BuildInfo, w io.Writer) error {
-	if bi == nil {
-		return errors.New("Could not show the build settings - no build info")
+// showSettings shows the build settings of this executable
+func showSettings(w io.Writer, bi *debug.BuildInfo) {
+	if !shortDisplay {
+		fmt.Fprintln(w, "Build Settings:")
 	}
-	fmt.Fprintln(w, "Build Settings:")
+
 	maxKey := 0
 	for _, s := range bi.Settings {
 		if len(s.Key) > maxKey {
 			maxKey = len(s.Key)
 		}
 	}
-	rpt := col.NewReport(nil, w,
-		col.New(&colfmt.String{StrJust: col.Right, W: maxKey},
-			"Key"),
+
+	keyJust := col.Right
+
+	hdr := col.NewHeaderOrPanic()
+	if shortDisplay {
+		col.HdrOptDontPrint(hdr)
+		keyJust = col.Left
+	}
+
+	rpt := col.NewReport(hdr, w,
+		col.New(&colfmt.String{StrJust: keyJust, W: maxKey}, "Key"),
 		col.New(&colfmt.String{}, "Value"))
-	fmt.Fprintln(w, "Build Settings:")
+
 	for _, s := range bi.Settings {
 		_ = rpt.PrintRow(s.Key, s.Value)
 	}
-	return nil
 }
 
-// showVersionPart shows the selected part of the version details
-func showVersionPart(w io.Writer) error {
+// showRaw shows the build info in raw form
+func showRaw(w io.Writer, bi *debug.BuildInfo) {
+	fmt.Fprintln(w, bi)
+}
+
+// showVersion shows the version details, if specific parts have been
+// requested then just those parts are shown otherwise the full default
+// version info is displayed
+func showVersion(w io.Writer) error {
+	if len(vsnPart) == 0 {
+		return nil
+	}
+
 	bi, ok := debug.ReadBuildInfo()
 	if !ok {
 		return errors.New(noBuildInfo)
 	}
 
-	var err error
-	partShown := false
+	shown := make(map[string]bool, len(vsnPart))
 	for _, part := range vsnPart {
-		switch part {
-		case vpGoVersion:
-			err = showGoVersion(bi, w)
-		case vpPath:
-			err = showPath(bi, w)
-		case vpMain:
-			err = showMain(bi, w)
-		case vpMods:
-			err = showModules(bi, w)
-		case vpSettings:
-			err = showSettings(bi, w)
-		default:
-			badVsnPart := "bad version part: " + part
-			err = errors.New(badVsnPart)
-			fmt.Fprintln(w, badVsnPart)
+		if shown[part] {
+			continue
 		}
-		partShown = true
+		shown[part] = true
+
+		var f versionPartShowFunc
+		var ok bool
+		if f, ok = vsnPartShowFuncMap[part]; !ok {
+			return errors.New("bad version part: " + part)
+		}
+
+		f(w, bi)
 	}
-	if partShown {
-		os.Exit(0)
-	}
-	return err
+
+	os.Exit(0)
+	return nil
 }
 
 // AddParams will add parameters to the passed param.PSet
 func AddParams(ps *param.PSet) error {
-	ps.Add("version", psetter.Nil{},
-		"show the version details for this program",
+	const (
+		paramNameVersion          = "version"
+		paramNameVersionPart      = "version-part"
+		paramNameVersionPartShort = "version-part-short"
+	)
+
+	defaultParts := []string{vpGoVersion, vpPath, vpMain, vpMods, vpSettings}
+
+	ps.Add(paramNameVersion, psetter.Nil{},
+		"show the complete version details for this program"+
+			" in the default format",
 		param.PostAction(
 			func(_ location.L, p *param.ByName, _ []string) error {
-				if bi, ok := debug.ReadBuildInfo(); !ok {
-					fmt.Fprintln(p.StdWriter(), noBuildInfo)
-				} else {
-					fmt.Fprintln(p.StdWriter(), bi.String())
-				}
-				os.Exit(0)
+				vsnPart = append(vsnPart, defaultParts...)
 				return nil
 			}),
+		param.SeeAlso(paramNameVersionPart),
 		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
 	)
 
-	ps.Add("version-part",
+	ps.Add(paramNameVersionPart,
 		psetter.EnumList{
 			Value: &vsnPart,
 			AllowedVals: psetter.AllowedVals{
-				vpGoVersion: "show the version of Go that produced this binary",
+				vpGoVersion: "show the version of Go used to make this program",
 				vpPath:      "show the path of the main package",
 				vpMain:      "show the version of the main module",
 				vpMods:      "show the module dependencies",
-				vpSettings:  "show other information about the build",
+				vpSettings:  "show the settings used to make this program",
+				vpRaw:       "show the full build information",
+			},
+			Aliases: psetter.Aliases{
+				"go":          []string{vpGoVersion},
+				"go-vsn":      []string{vpGoVersion},
+				"mods":        []string{vpMods},
+				"dep":         []string{vpMods},
+				"build-flags": []string{vpSettings},
+				"build":       []string{vpSettings},
+				"settings":    []string{vpSettings},
+				"default":     defaultParts,
 			},
 		},
-		"show the named part of the version",
+		"show only the named parts of the version",
+		param.SeeAlso(paramNameVersionPartShort),
+		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
+	)
+
+	ps.Add(paramNameVersionPartShort,
+		psetter.Bool{Value: &shortDisplay},
+		"show the version parts in simplified form, without headings and"+
+			" prompts. This is more useful if you want to use the value"+
+			" as you won't need to strip out the other text. Note that"+
+			" there is no short form of the '"+vpRaw+"' form.",
+		param.SeeAlso(paramNameVersionPart),
 		param.Attrs(param.CommandLineOnly|param.DontShowInStdUsage),
 	)
 
 	ps.AddFinalCheck(func() error {
-		return showVersionPart(ps.StdW())
+		return showVersion(ps.StdW())
 	})
 
 	return nil
